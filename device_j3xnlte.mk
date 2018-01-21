e5a0d93706b01f01b0bc9c36b4e6bb38865e212f
Danijel Jovanovic <30354085+danijelj2001@users.noreply.github.com>
2018-01-21 08:29:17 +0100

    Update device_j3xnlte.mk

--- device_j3xnlte.mk
+++ device_j3xnlte.mk
@@ -4,8 +4,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/j3xnlte/device.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_j3xnlte
-PRODUCT_DEVICE := j3xnlte
+PRODUCT_NAME := full_j3xnlte/j2xlte
+PRODUCT_DEVICE := j3xnlte/j2xlte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
